1ce1e596ba9954b82255bd28220da1494618e2fb
Samantha Barros <samantha.barros95@gmail.com>
2020-06-28 02:24:08 +0000

    Automatically backed up by Learn

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -8,8 +8,9 @@ class GetRequester
  
  attr_accessor :GetRequester
  
- 
+ def initialize()
  URL = "https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json"
+ end 
  
   def get_response_body
     
